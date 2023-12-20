b69ff82c5345c1acdcc20f81597805b65b9c378a
Hyungchul You <teoyou881@gmail.com>
2023-12-20 16:41:01 -0500

    feat(DoubleLinkedList Main)

---
+++ src/dataStrucrues/doublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,48 @@
+package dataStrucrues.doublyLinkedList;
+
+public class DoublyLinkedList {
+
+  private Node head;
+  private Node tail;
+  private int length;
+
+  class Node {
+
+    int value;
+    Node next;
+    Node prev;
+
+    Node(int value) {
+      this.value = value;
+    }
+  }
+
+  public DoublyLinkedList(int value) {
+    Node newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+  }
+
+  public void getHead() {
+    System.out.println("head = " + head.value);
+  }
+
+  public void getTail() {
+    System.out.println("tail = " + tail.value);
+  }
+
+  public void getLength() {
+    System.out.println("length = " + length);
+  }
+
+  public void printList() {
+    Node currentNode = this.head;
+    while (currentNode != null) {
+      System.out.println(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+
+
+}
---
+++ src/dataStrucrues/doublyLinkedList/Main.java
@@ -0,0 +1,6 @@
+package dataStrucrues.doublyLinkedList;
+public class Main {
+
+  public static void main(String[] args) {
+  }
+}
