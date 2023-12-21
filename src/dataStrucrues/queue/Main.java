9b5b7f52667d9de67ba5e07bb97824bcd9f7a71d
Hyungchul You <teoyou881@gmail.com>
2023-12-21 16:33:01 -0500

    feat(Queue Main): add dequeue method

--- src/dataStrucrues/queue/Main.java
+++ src/dataStrucrues/queue/Main.java
@@ -4,49 +4,22 @@ public class Main {
   public static void main(String[] args) {
 
     Queue myQueue = new Queue(2);
-
-    System.out.println("Before enqueue():");
-    System.out.println("--------------");
-    myQueue.getFirst();
-    myQueue.getLast();
-    myQueue.getLength();
-
-    System.out.println("\nQueue:");
-    myQueue.printQueue();
-
     myQueue.enqueue(1);
 
-    System.out.println("\n\nAfter enqueue():");
-    System.out.println("-------------");
-    myQueue.getFirst();
-    myQueue.getLast();
-    myQueue.getLength();
+    // (2) Items - Returns 2 Node
+    System.out.println(myQueue.dequeue().value);
+    // (1) Item - Returns 1 Node
+    System.out.println(myQueue.dequeue().value);
+    // (0) Items - Returns null
+    System.out.println(myQueue.dequeue());
 
-    System.out.println("\nQueue:");
-    myQueue.printQueue();
 
         /*
             EXPECTED OUTPUT:
-
-            Before enqueue():
-            --------------
-            First: 2
-            Last: 2
-            Length: 1
-
-            Queue:
-            2
-
-
-            After enqueue():
-            -------------
-            First: 2
-            Last: 1
-            Length: 2
-
-            Queue:
+            ----------------
             2
             1
+            null
 
         */
 
--- src/dataStrucrues/queue/Queue.java
+++ src/dataStrucrues/queue/Queue.java
@@ -1,24 +1,28 @@
 package dataStrucrues.queue;
 
 public class Queue {
+
   private Node first;
   private Node last;
   private int length;
 
   class Node {
+
     int value;
     Node next;
 
     Node(int value) {
-      this.value   = value;
+      this.value = value;
     }
   }
+
   public Queue(int value) {
     Node newNode = new Node(value);
     first = newNode;
     last = newNode;
-    length =1;
+    length = 1;
   }
+
   public void printQueue() {
     Node temp = first;
     while (temp != null) {
@@ -28,26 +32,39 @@ public class Queue {
   }
 
   public void getFirst() {
-    System.out.println("first = "+ first.value);
+    System.out.println("first = " + first.value);
   }
+
   public void getLast() {
-    System.out.println("last ="+last.value);
+    System.out.println("last =" + last.value);
   }
+
   public void getLength() {
     System.out.println("length = " + length);
   }
 
   public void enqueue(int value) {
     Node newNode = new Node(value);
-    if(length==0){
+    if (length == 0) {
       this.first = newNode;
       this.last = newNode;
-    }else{
-    Node temp = this.last;
-    this.last = newNode;
-    temp.next = newNode;
-
+    } else {
+      Node temp = this.last;
+      this.last = newNode;
+      temp.next = newNode;
     }
     length++;
+  }
+
+  public Node dequeue() {
+    if (length == 0) {
+      return null;
+    }
+    Node temp = this.first;
+    this.first = this.first.next;
+    temp.next = null;
+
+    length--;
+    return temp;
   }
 }
