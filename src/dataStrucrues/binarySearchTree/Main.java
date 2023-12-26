a272e352aa9d297931de5edb30b5f1a29c7468b7
Hyungchul You <teoyou881@gmail.com>
2023-12-25 23:57:54 -0500

    feat(BST: Insert)

--- src/dataStrucrues/binarySearchTree/Main.java
+++ src/dataStrucrues/binarySearchTree/Main.java
@@ -7,7 +7,34 @@ public class Main {
 
     BinarySearchTree myBST = new BinarySearchTree();
 
-    System.out.println("myBST.root = " + myBST.root);
+    myBST.insert(2);
+    myBST.insert(1);
+    myBST.insert(3);
+
+        /*
+            THE LINES ABOVE CREATE THIS TREE:
+                         2
+                        / \
+                       1   3
+        */
+
+
+    // ROOT MUST BE PUBLIC FOR THESE LINES TO WORK
+    System.out.println("Root: " + myBST.root.value);
+    System.out.println("\nRoot->Left: " + myBST.root.left.value);
+    System.out.println("\nRoot->Right: " + myBST.root.right.value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Root: 2
+
+            Root->Left: 1
+
+            Root->Right: 3
+
+        */
 
   }
 
